c955a65171c7650e6b56cdc2f9bc33bf413cf12d
Lucas Queiroz Oliveira <lucas.q.oliveira@accenture.com>
2026-07-13 16:37:40 -0300

    primeiro commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// ── Supabase Credentials ──
+// 1. Acesse https://supabase.com e crie um projeto
+// 2. Vá em Project Settings → API
+// 3. Cole a "Project URL" e a "anon public" key abaixo
+
+const SUPABASE_URL = 'https://ivabhhyvoaqlbuouaiya.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2YWJoaHl2b2FxbGJ1b3VhaXlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NjIyMjksImV4cCI6MjA5OTUzODIyOX0.1X-W2Ess5Mcmy-kzJ9bg_MKCvhHPHLQVeYgJcoxAGII';
